cd28c988763c9202647d14e8235329269cc7598c
Merdan Kurbanov <212merdan@gmail.com>
2023-07-21 14:15:24 +0300

    db connections setting

--- Koton.ECommerce.DataAccess/Repositories/Concrete/ProductRepository.cs
+++ Koton.ECommerce.DataAccess/Repositories/Concrete/ProductRepository.cs
@@ -27,8 +27,10 @@ namespace Koton.ECommerce.DataAccess.Repositories.Concrete
         }
 
         public async Task<GetProductDto> GetProductById(int id)
+
         {
-            using (var connection = new SqlConnection("Server=DESKTOP-UIC5F07\\SQLEXPRESS;Database=kotonECommerce;Integrated Security=true;MultipleActiveResultSets=True;TrustServerCertificate=true;Application Name=Koton.ECommerce"))
+            var connStr = _config.GetSection("ConnectionStrings:ConnStr").Value;
+            using (var connection = new SqlConnection(connStr))
             {
                 // Retrieve the product with the specified id.
                 var product = await connection.QueryFirstOrDefaultAsync<GetProductDto>("SELECT * FROM Products WHERE Id = @Id", new { Id = id });
@@ -39,7 +41,8 @@ namespace Koton.ECommerce.DataAccess.Repositories.Concrete
 
         public async Task<GetProductDto> AddProduct(CreateProductDto product)
         {
-            using (var connection = new SqlConnection("Server=DESKTOP-UIC5F07\\SQLEXPRESS;Database=kotonECommerce;Integrated Security=true;MultipleActiveResultSets=True;TrustServerCertificate=true;Application Name=Koton.ECommerce"))
+            var connStr = _config.GetSection("ConnectionStrings:ConnStr").Value;
+            using (var connection = new SqlConnection(connStr))
             {
                 // Insert the new product into the database.
                 //var productId = await connection.ExecuteScalarAsync<int>("INSERT INTO Products (Id,Name,Price,DiscountPrice, Description,ImageUrl,Brand,UserId ) VALUES (@Id,@Name,@Price,@DiscountPrice, @Description,@ImageUrl,@Brand,@UserId); SELECT SCOPE_IDENTITY();", product);
@@ -55,7 +58,8 @@ namespace Koton.ECommerce.DataAccess.Repositories.Concrete
 
         public async Task<GetProductDto> UpdateProduct(int id, UpdateProductDto product)
         {
-            using (var connection = new SqlConnection("Server=DESKTOP-UIC5F07\\SQLEXPRESS;Database=kotonECommerce;Integrated Security=true;MultipleActiveResultSets=True;TrustServerCertificate=true;Application Name=Koton.ECommerce"))
+            var connStr = _config.GetSection("ConnectionStrings:ConnStr").Value;
+            using (var connection = new SqlConnection(connStr))
             {
                 // Update the product in the database.
                 await connection.ExecuteAsync("UPDATE Products SET Name = @Name, Description = @Description, Price = @Price WHERE Id = @Id",
@@ -70,7 +74,8 @@ namespace Koton.ECommerce.DataAccess.Repositories.Concrete
 
         public async Task DeleteProduct(int id)
         {
-            using (var connection = new SqlConnection("Server=DESKTOP-UIC5F07\\SQLEXPRESS;Database=kotonECommerce;Integrated Security=true;MultipleActiveResultSets=True;TrustServerCertificate=true;Application Name=Koton.ECommerce"))
+            var connStr = _config.GetSection("ConnectionStrings:ConnStr").Value;
+            using (var connection = new SqlConnection(connStr))
             {
                 // Delete the product from the database.
                 await connection.ExecuteAsync("DELETE FROM Products WHERE Id = @Id", new { Id = id });
