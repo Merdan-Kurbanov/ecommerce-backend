2749c85a80fe3ec23907f5da6986481c438488c0
Merdan Kurbanov <212merdan@gmail.com>
2023-07-23 17:13:52 +0300

    Login rep modified to get role data

--- Koton.ECommerce.DataAccess/Repositories/Concrete/LoginRepository.cs
+++ Koton.ECommerce.DataAccess/Repositories/Concrete/LoginRepository.cs
@@ -2,23 +2,37 @@
 using Koton.ECommerce.Core.DTOs;
 using Koton.ECommerce.DataAccess.Repositories.Abstract;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace Koton.ECommerce.DataAccess.Repositories.Concrete
 {
     public class LoginRepository : ILoginRepository
     {
+        private readonly IConfiguration _config;
+
+        public LoginRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public async Task<UserInfoDto> GetUserInfoAsync(string username, string password)
         {
-            using (var connection = new SqlConnection("Server=DESKTOP-UIC5F07\\SQLEXPRESS;Database=kotonECommerce;Integrated Security=true;MultipleActiveResultSets=True;TrustServerCertificate=true;Application Name=Koton.ECommerce"))
+            var connStr = _config.GetSection("ConnectionStrings:ConnStr").Value;
+
+            using (var connection = new SqlConnection(connStr))
             {
                 // Filter the query based on the provided username and password.
                 var userInfo = await connection.QueryFirstOrDefaultAsync<UserInfoDto>(
-                    sql: "SELECT Username, PasswordHash, Email FROM Users WHERE Username = @Username AND PasswordHash = @PasswordHash",
+                    sql: @"SELECT u.Username, u.PasswordHash, u.Email, r.Code as RoleCode, r.Name as RoleName
+                   FROM Users u
+                   INNER JOIN Roles r ON u.RoleId = r.RoleId
+                   WHERE u.Username = @Username AND u.PasswordHash = @PasswordHash",
                     param: new { Username = username, PasswordHash = password },
                     commandTimeout: 0);
 
                 return userInfo;
             }
         }
+
     }
 }
